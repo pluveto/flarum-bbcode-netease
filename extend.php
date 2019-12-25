6b47bf0ddfe6e610c6710f68987117edebe1fce4
pluveto <50045289+pluveto@users.noreply.github.com>
2019-12-25 23:16:14 +0800

    Create extend.php

---
+++ extend.php
@@ -0,0 +1,12 @@
+namespace Pluveto\netease;
+use Flarum\Extend;
+use s9e\TextFormatter\Configurator;
+return [
+    (new Extend\Formatter)
+        ->configure(function (Configurator $config) {
+             $config->BBCodes->addCustom(
+               '[bilibili id={NUMBER1?}}][/bilibili]',
+               '<iframe src="http://music.163.com/outchain/player?type=0&amp;id={NUMBER1}&amp;auto=0&amp;height=430" width="100%" height="450" frameborder="no" marginwidth="0" marginheight="0"></iframe> 
+            );
+        })
+];
