af0bad93eed59344e08ec51c03cbf41d9bb67c8e
pluveto <50045289+pluveto@users.noreply.github.com>
2019-12-25 23:23:58 +0800

    Update extend.php

--- extend.php
+++ extend.php
@@ -6,7 +6,7 @@ return [
         ->configure(function (Configurator $config) {
              $config->BBCodes->addCustom(
                '[bilibili id={NUMBER1?}}][/bilibili]',
-               '<iframe src="http://music.163.com/outchain/player?type=0&amp;id={NUMBER1}&amp;auto=0&amp;height=430" width="100%" height="450" frameborder="no" marginwidth="0" marginheight="0"></iframe> 
+               '<iframe src="http://music.163.com/outchain/player?type=0&amp;id={NUMBER1}&amp;auto=0&amp;height=430" width="100%" height="450" frameborder="no" marginwidth="0" marginheight="0"></iframe>'
             );
         })
 ];
