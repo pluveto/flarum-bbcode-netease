66ae42f29ba584da95a9b8b6654e619f03f176a2
pluveto <50045289+pluveto@users.noreply.github.com>
2019-12-28 16:04:57 +0800

    Update extend.php

--- extend.php
+++ extend.php
@@ -1,13 +1,13 @@
 <?php
-namespace Pluveto\netease;
-use Flarum\Extend;
-use s9e\TextFormatter\Configurator;
-return [
+namespace Pluveto\netease;                                                           
+use Flarum\Extend;                                                                   
+use s9e\TextFormatter\Configurator;                                                  
+return [                                                                             
     (new Extend\Formatter)
         ->configure(function (Configurator $config) {
              $config->BBCodes->addCustom(
-               '[bilibili id={NUMBER1?}}][/bilibili]',
-               '<iframe src="http://music.163.com/outchain/player?type=0&amp;id={NUMBER1}&amp;auto=0&amp;height=430" width="100%" height="450" frameborder="no" marginwidth="0" marginheight="0"></iframe>'
+               '[netease id={NUMBER1?}][/netease]',
+               '<p><iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width=330 height=86 src="https://music.163.com/outchain/player?type=2&id={NUMBER1}&auto=0&height=66"></iframe></p>'
             );
-        })
+        })     
 ];
